ae2dd7a0f58185082e25c322f63dbafe28a0800f
DARVCODEV <rojasvidaldaniel@gmail.com>
2024-03-16 07:55:02 -0500

    React Query

---
+++ 01-cripto-random/src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { App } from "./App.tsx";
+import "./index.css";
+
+// Create a client
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </QueryClientProvider>
+);
