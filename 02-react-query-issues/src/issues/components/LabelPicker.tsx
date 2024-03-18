00041e78b99970daccff8cca27f865437ee1b327
DARVCODEV <rojasvidaldaniel@gmail.com>
2024-03-18 11:17:02 -0500

    feat: :zap: SelectedLabels

--- 02-react-query-issues/src/issues/components/LabelPicker.tsx
+++ 02-react-query-issues/src/issues/components/LabelPicker.tsx
@@ -1,7 +1,12 @@
 import { LoadingIcon } from "../../shared/components/LoadingIcon";
 import { useLabels } from "../hooks/useLabels";
 
-export const LabelPicker = () => {
+interface Props {
+  selectedLabels: string[];
+  onChange: (labelName: string) => void;
+}
+
+export const LabelPicker: React.FC<Props> = ({ selectedLabels, onChange }) => {
   const { labelsQuery } = useLabels();
 
   if (labelsQuery.isLoading) {
@@ -13,11 +18,12 @@ export const LabelPicker = () => {
       {labelsQuery.data?.map((label) => (
         <span
           key={label.id}
-          className="badge rounded-pill m-1 label-picker"
+          className={`badge rounded-pill m-1 label-picker ${ selectedLabels.includes(label.name) ? "label-active" : ""}`}
           style={{
             border: `1px solid #${label.color}`,
             color: `#${label.color}`,
           }}
+          onClick={() => onChange(label.name)}
         >
           {label.name}
         </span>
--- 02-react-query-issues/src/issues/views/ListView.tsx
+++ 02-react-query-issues/src/issues/views/ListView.tsx
@@ -1,18 +1,28 @@
-import { IssueList } from '../components/IssueList';
-import { LabelPicker } from '../components/LabelPicker';
-
+import { useState } from "react";
+import { IssueList } from "../components/IssueList";
+import { LabelPicker } from "../components/LabelPicker";
 
 export const ListView = () => {
+  const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
+
+  const onLabelChange = (labelName: string) => {
+    selectedLabels.includes(labelName)
+      ? setSelectedLabels(selectedLabels.filter((label) => label !== labelName))
+      : setSelectedLabels([...selectedLabels, labelName]);
+  };
+
   return (
     <div className="row mt-5">
-      
       <div className="col-8">
         <IssueList />
       </div>
-      
+
       <div className="col-4">
-        <LabelPicker />
+        <LabelPicker
+          selectedLabels={selectedLabels}
+          onChange={(labelName) => onLabelChange(labelName)}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
